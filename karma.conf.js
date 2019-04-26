240d8f7021c291767c3b08cce91d09c9351865b0
FlikHu <hah045@ucsd.edu>
2019-04-26 16:17:42 -0700

    test

--- karma.conf.js
+++ karma.conf.js
@@ -48,6 +48,13 @@ module.exports = function (config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['ChromeHeadless'],
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
